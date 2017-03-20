546b154252dcd71712c5b2f764727e62aeb31943
Chaitanya Ghone <csghone@acceletrade.com>
2017-03-20 10:02:05 +0000

    [Ubuntu] Add Byobu with PPA
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- packages/ubuntu/scripts/install_packages_misc.sh
+++ packages/ubuntu/scripts/install_packages_misc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo dpkg --add-architecture i386
+sudo add-apt-repository -y ppa:byobu/ppa
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
@@ -19,4 +20,5 @@ if [ $1 == "install" ]; then
     sudo -E apt-get install -y curl corkscrew
     sudo -E apt-get install -y tree htop tilda screen terminator
     sudo -E apt-get install -y ssh sshfs
+    sudo -E apt-get install -y byobu tmux
 fi
