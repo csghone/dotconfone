2d3b36efe6962aa007e27731b65917c3b994d15a
Chaitanya Ghone <csghone@acceletrade.com>
2017-03-20 10:03:07 +0000

    [Ubuntu] Add VIM latest PPA
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- packages/ubuntu/scripts/install_packages_misc.sh
+++ packages/ubuntu/scripts/install_packages_misc.sh
@@ -2,6 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo add-apt-repository -y ppa:byobu/ppa
+sudo add-apt-repository -y ppa:jonathonf/vim
 
 if [ $1 == "install" ]; then
     sudo -E apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
