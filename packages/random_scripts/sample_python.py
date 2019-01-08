10d17dc1d0915ec87ff182584f41d56511694fb9
Chaitanya Ghone <csghone@acceletrade.com>
2019-01-08 19:31:39 +0530

    Update python sample

--- packages/random_scripts/sample_python.py
+++ packages/random_scripts/sample_python.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """
 eg:
-./sample_python.py -w -x opt1 -y 3 -z 1 2 3
-
+./sample_python.py -w -x opt1 -y 3 -z 1 2 3 -f ~/.bashrc
 """
 from __future__ import print_function
 
@@ -18,25 +17,37 @@ import random
 import retrying
 
 
-logger = logging.getLogger()
+# Use these two lines in all files
+logger = logging.getLogger(__name__)
+logger.propagate = True
+
+
+# Call setup_logging() only in file with def main()
+# LOG_FORMATTER and def setup_logging() can be moved to a common file for reuse.
 LOG_FORMATTER = logging.Formatter(
     "%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - " +
     "%(lineno)s - %(funcName)s - " +
     "%(message)s",
     "%Y%m%d %H:%M:%S")
 
-def setup_logging(level=logging.INFO, enable_console=True):
+
+def setup_logging(inp_file, level=logging.INFO, enable_console=True):
     file_log_handler = logging.handlers.RotatingFileHandler(
-        "__" + os.path.basename(__file__) + ".main__" + ".log",
+        "__" + os.path.basename(inp_file) + ".main__" + ".log",
         maxBytes=1000000,
         backupCount=5)
     console_log_handler = logging.StreamHandler()
-    logger.addHandler(file_log_handler)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(file_log_handler)
     if enable_console:
-        logger.addHandler(console_log_handler)
-    logger.setLevel(level)
-    for handler in logging.root.handlers:
+        root_logger.addHandler(console_log_handler)
+    root_logger.setLevel(level)
+    for handler in logger.root.handlers:
         handler.setFormatter(fmt=LOG_FORMATTER)
+
+
+class CustomException(Exception):
+    pass
 
 
 def retry_on_result_check(result):
@@ -50,19 +61,20 @@ def retry_on_exception_check(exception):
     logger.error("\n%s", exc_mesg)
     logger.error("Error: %s", exception)
     # Return value should be True (retry) or False (don't retry)
-    return isinstance(exception, NameError)
+    return isinstance(exception, CustomException)
 
 
+# wait_fixed is in milliseconds
 @retrying.retry(wait_fixed=1000,
+                stop_max_attempt_number=5,
                 retry_on_result=retry_on_result_check,
                 retry_on_exception=retry_on_exception_check)
 def retry_example():
     if random.randrange(1, 10) > 3:
-        print(a) # Throws NameError
+        raise CustomException("CustomException occured")
     else:
         print("Avoided exception in retry_example")
     return random.randrange(1, 10)
-
 
 
 def print_variables(**kwargs):
@@ -78,6 +90,9 @@ def process(**kwargs):
 
     print_variables(**kwargs)
 
+    print("First line in '{}':".format(inp_file.name))
+    print(inp_file.readline())
+
     logger.debug("Message: %s", "debug")
     logger.info("Message: %s, %s", "info", 1)
     logger.warning("warning")
@@ -85,7 +100,16 @@ def process(**kwargs):
     logger.critical("critical")
 
     logger.info("Example usage of retrying module - start")
-    retry_example()
+    try:
+        retry_example()
+    except CustomException:
+        print("Ignoring final exception")
+    except retrying.RetryError as error:
+        print("Max retries reached",
+              "Attempt:", error.last_attempt.attempt_number,
+              "Value:", error.last_attempt.value,
+              "Has Exception:", error.last_attempt.has_exception)
+
     logger.info("Example usage of retrying module - end")
 
     return 0
@@ -129,6 +153,14 @@ def main():
         type=int,
         default=[]
     )
+    parser.add_argument(
+        "-f",
+        "--inp_file",
+        dest="inp_file",
+        help="Input file",
+        type=argparse.FileType(mode='r'),
+        required=True
+    )
 
     myargs = parser.parse_args()
 
@@ -136,7 +168,7 @@ def main():
 
 
 if __name__ == '__main__':
-    setup_logging(level=logging.INFO)
+    setup_logging(__file__, level=logging.INFO)
     try:
         sys.exit(main()) # Ensure return value is passed to shell
     except Exception as error: # pylint: disable=W0702, W0703
